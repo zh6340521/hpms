bd5587520a2a0da86f323c8baaaaf05c60d4171c
10988 <feng.liu01@hand-china.com>
2017-02-16 19:25:23 +0800

    基本完成 费项类型 任务

--- core/src/main/java/hpms/mdm/controllers/FeeTypeController.java
+++ core/src/main/java/hpms/mdm/controllers/FeeTypeController.java
@@ -11,35 +11,37 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
-    @Controller
-    public class FeeTypeController extends BaseController{
+@Controller
+@RequestMapping(value = "/mdm/feetype")
+public class FeeTypeController extends BaseController {
 
     @Autowired
     private IFeeTypeService service;
 
 
-    @RequestMapping(value = "/hpms/mdm/fee/type/query")
+    @RequestMapping(value = "/query")
     @ResponseBody
     public ResponseData query(FeeType dto, @RequestParam(defaultValue = DEFAULT_PAGE) int page,
-        @RequestParam(defaultValue = DEFAULT_PAGE_SIZE) int pageSize, HttpServletRequest request) {
+                              @RequestParam(defaultValue = DEFAULT_PAGE_SIZE) int pageSize, HttpServletRequest request) {
         IRequest requestContext = createRequestContext(request);
-        return new ResponseData(service.select(requestContext,dto,page,pageSize));
+        return new ResponseData(service.select(requestContext, dto, page, pageSize));
     }
 
-    @RequestMapping(value = "/hpms/mdm/fee/type/submit")
+    @RequestMapping(value = "/submit")
     @ResponseBody
-    public ResponseData update(HttpServletRequest request,@RequestBody List<FeeType> dto){
+    public ResponseData update(HttpServletRequest request, @RequestBody List<FeeType> dto) {
         IRequest requestCtx = createRequestContext(request);
         return new ResponseData(service.batchUpdate(requestCtx, dto));
     }
 
-    @RequestMapping(value = "/hpms/mdm/fee/type/remove")
+    @RequestMapping(value = "/remove")
     @ResponseBody
-    public ResponseData delete(HttpServletRequest request,@RequestBody List<FeeType> dto){
+    public ResponseData delete(HttpServletRequest request, @RequestBody List<FeeType> dto) {
         service.batchDelete(dto);
         return new ResponseData();
     }
-    }
+}
