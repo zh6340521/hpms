4a50a1fe9189441f44499870bcb216ca4e0196ab
10988 <feng.liu01@hand-china.com>
2017-02-16 20:23:20 +0800

    修复 费项类型 界面更新时唯一性验证错误的bug

--- core/src/main/java/hpms/mdm/service/impl/FeeTypeServiceImpl.java
+++ core/src/main/java/hpms/mdm/service/impl/FeeTypeServiceImpl.java
@@ -6,6 +6,13 @@ import hpms.mdm.dto.FeeType;
 import hpms.mdm.service.IFeeTypeService;
 import org.springframework.transaction.annotation.Transactional;
 
+/**
+ * @name hpms.mdm.dto.FeeType
+ * @description 费项类型 service 接口实现类
+ * @author feng.liu01@hand-china.com 2017/02/16
+ * @version 1.0
+ */
+
 @Service
 @Transactional
 public class FeeTypeServiceImpl extends BaseServiceImpl<FeeType> implements IFeeTypeService{
