9f2c475d5648272eef740435261cf85a27fcb46e
hufuchun <fuchun.hu@hand-china.com>
2017-02-28 17:28:01 +0800

    提交了基础数据的部分代码

---
+++ core/src/main/java/hpms/bs/dto/DataModel.java
@@ -0,0 +1,86 @@
+package hpms.bs.dto;/**
+ * Created by user1 on 2017/2/28.
+ */
+
+import com.hand.hap.system.dto.BaseDTO;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * @author fuchun.hu@hand-china.com
+ * @version 1.0
+ * @name DataModel
+ * @description
+ * @date 2017/2/28
+ */
+@Table(name = "HPMS_DATA_MODEL")
+public class DataModel extends BaseDTO {
+
+    /*model_id              NUMBER not null,
+    model_code            VARCHAR2(30) not null,
+    model_name            VARCHAR2(240) not null,
+    table_name            VARCHAR2(30) not null,*/
+
+    private static final long serialVersionUID = 2856108923186548186L;
+
+    /**
+     * 表ID，主键，供其他表做外键
+     */
+    @Id
+    @Column
+    @GeneratedValue(generator = GENERATOR_TYPE)
+    private Long  modelId;
+
+    /**
+     * 编码
+     */
+    @Column
+    private String modelCode;
+
+    /**
+     * 模型名称
+     */
+    @Column
+    private String modelName;
+
+    /**
+     * 数据表名
+     */
+    @Column
+    private String tableName;
+
+    public Long getModelId() {
+        return modelId;
+    }
+
+    public void setModelId(Long modelId) {
+        this.modelId = modelId;
+    }
+
+    public String getModelCode() {
+        return modelCode;
+    }
+
+    public void setModelCode(String modelCode) {
+        this.modelCode = modelCode;
+    }
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+
+    public String getTableName() {
+        return tableName;
+    }
+
+    public void setTableName(String tableName) {
+        this.tableName = tableName;
+    }
+}
---
+++ core/src/main/java/hpms/bs/mapper/DataModelMapper.java
@@ -0,0 +1,16 @@
+package hpms.bs.mapper;/**
+ * Created by user1 on 2017/2/28.
+ */
+
+import com.hand.hap.mybatis.common.Mapper;
+import hpms.bs.dto.DataModel;
+
+/**
+ * @author fuchun.hu@hand-china.com
+ * @version 1.0
+ * @name DataModelMapper
+ * @description
+ * @date 2017/2/28
+ */
+public interface DataModelMapper extends Mapper<DataModel> {
+}
