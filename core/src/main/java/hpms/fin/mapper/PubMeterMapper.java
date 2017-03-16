f76f1e637d121a2fe5978cae10238a69a79e3c77
赵俊 <jun.zhao02@hand-china.com>
2017-03-16 15:25:06 +0800

    【公表抄表】换表代码提交

--- core/src/main/java/hpms/fin/mapper/PubMeterMapper.java
+++ core/src/main/java/hpms/fin/mapper/PubMeterMapper.java
@@ -7,4 +7,6 @@ import java.util.List;
 public interface PubMeterMapper extends Mapper<PubMeter>{
 
     List<PubMeter> queryPubMeter(PubMeter pubMeter);
+    List<PubMeter> queryPubMeterInit(PubMeter pubMeter);
+    List<PubMeter> queryPubMeterChange(PubMeter pubMeter);
 }
