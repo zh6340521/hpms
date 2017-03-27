647517516ed17627727da9d0d7931e456eee3beb
郑鹏飞 <pengfei.zheng@hand-china.com>
2017-03-27 17:27:45 +0800

    完善仪表添加房间信息提示以及显示刷新

---
+++ core/src/main/java/hpms/mdm/mapper/PurchaseOrgMapper.java
@@ -0,0 +1,12 @@
+package hpms.mdm.mapper;
+
+import com.hand.hap.mybatis.common.Mapper;
+import hpms.mdm.dto.PurchaseOrg;
+
+/**
+ * Created by LoseMyself
+ * on 2017/3/27   16:41
+ */
+public interface PurchaseOrgMapper extends Mapper<PurchaseOrg> {
+
+}
